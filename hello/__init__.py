aa88b5501ee44eb3bcd3e8de58830226c1503472
Felix Zumstein <fzumstein@gmail.com>
2023-05-16 11:33:09 +0200

    use Book as context manager

--- hello/__init__.py
+++ hello/__init__.py
@@ -11,16 +11,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse("Unauthorized", status_code=401, mimetype="text/html")
 
     req_body = req.get_json()
-    book = xw.Book(json=req_body)
+    with xw.Book(json=req_body) as book:
 
-    sheet = book.sheets[0]
-    if sheet["A1"].value == "Hello xlwings!":
-        sheet["A1"].value = "Bye xlwings!"
-    else:
-        sheet["A1"].value = "Hello xlwings!"
+        sheet = book.sheets[0]
+        if sheet["A1"].value == "Hello xlwings!":
+            sheet["A1"].value = "Bye xlwings!"
+        else:
+            sheet["A1"].value = "Hello xlwings!"
 
-    return func.HttpResponse(
-        json.dumps(book.json()),
-        status_code=200,
-        mimetype="application/json",
-    )
+        return func.HttpResponse(
+            json.dumps(book.json()),
+            status_code=200,
+            mimetype="application/json",
+        )
